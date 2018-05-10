177386ecfc80552a31c328f4ea04790fab00bfea
Maksym Havryliuk <maks.havryliuk@gmail.com>
2018-05-10 12:00:31 +0300

    Practice on unit testing.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("20180405_UnitTesting.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("20180405_UnitTesting.Tests")]
+[assembly: AssemblyCopyright("Copyright © Maksym Havryliuk 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("418bf45d-2a78-4f84-ac82-3ebf4f68fce0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VectorTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/**<remark>
+ * Practice on unit testing.
+ * Modular testing of the previously created Vector application.
+ * Class Assert: https://msdn.microsoft.com/ru-ru/library/microsoft.visualstudio.testtools.unittesting.assert.aspx
+</remark> */
+
+namespace _20180405_UnitTesting.Tests
+{
+    [TestClass]
+    public class VectorTests
+    {
+        private Vector vector1;
+        private Vector vector2;
+
+        // Начальная инициализация для каждого отдельного теста.
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            vector1 = new Vector(2, 4, 6);
+            vector2 = new Vector(1, 3, 5);            
+        }
+
+        // Проверка перегрузки бинарного оператора +
+        [TestMethod]
+        public void AddVector()
+        {
+            // Arrange
+            Vector expected = new Vector(3, 7, 11);
+
+            // Act
+            Vector actual = vector1 + vector2;
+
+            // Assert
+            Assert.AreEqual(expected.ToString(), actual.ToString());   // Проверка значений на равенство
+        }
+
+        // Проверка перегрузки бинарного оператора -
+        [TestMethod]
+        public void SubVector()
+        {
+            // Arrange
+            Vector expected = new Vector(1, 1, 1);
+
+            // Act
+            Vector actual = vector1 - vector2;
+
+            // Assert
+            Assert.AreEqual(expected.ToString(), actual.ToString());   // Проверка значений на равенство
+        }
+
+        // Проверка, что векторы являются не null
+        [TestMethod]
+        public void IsNotNull()
+        {
+            Assert.IsNotNull(vector1);
+            Assert.IsNotNull(vector2);
+        }
+
+        // Проверяем тип входных данных
+        [TestMethod]
+        public void TypeIsDouble()
+        {
+            Assert.IsInstanceOfType(vector1.X, typeof(double));
+            Assert.IsInstanceOfType(vector1.Y, typeof(double));
+            Assert.IsInstanceOfType(vector1.Z, typeof(double));
+        }
+        
+        // Проверка на "условное" переполнение (исключение если значения всех координат < 0)
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void PointValueIsLessThanZero()
+        {
+            Vector vector3 = new Vector(-14, -105, -2);
+        }
+    }
+}
